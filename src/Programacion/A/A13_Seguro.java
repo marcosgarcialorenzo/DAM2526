994a641d0c303a454c02be475196b9bbc2bfb836
Marcos Garcia Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2025-10-29 08:45:06 +0100

    Add A13_Seguro and B5_SumaNumeros classes with discount calculation and sum logic

--- src/Programacion/A/A13_Seguro.java
+++ src/Programacion/A/A13_Seguro.java
@@ -1,6 +1,28 @@
 package Programacion.A;
 
 public class A13_Seguro {
+    int edad;
+    int agnoUltimoParte;
+    boolean cocheNuevo;
 
-    //esto es una prueba para ver si funcionaf
+    public static void main(String[] args) {
+        A13_Seguro seguro = new A13_Seguro();
+        seguro.edad = 31;
+        seguro.agnoUltimoParte = 3;
+        seguro.cocheNuevo = false;
+        System.out.println("Tiene descuento:" + seguro.concederDescuento());
+        /*if (seguro.concederDescuento()) {
+            System.out.println("Descuento concedido");
+        } else {
+            System.out.println("Descuento no concedido");
+        }*/
+    }
+
+    boolean concederDescuento() {
+        boolean descuento;
+        descuento = (edad >= 30 && agnoUltimoParte > 1 && cocheNuevo)
+                || (edad >= 31 && agnoUltimoParte > 2 && !cocheNuevo)
+                || (edad < 31 && agnoUltimoParte > 5);
+        return descuento;
+    }
 }
--- src/Programacion/B/B5_SumaNumeros.java
+++ src/Programacion/B/B5_SumaNumeros.java
@@ -1,4 +1,19 @@
 package Programacion.B;
 
 public class B5_SumaNumeros {
+    public static void main(String[] args) {
+        B5_SumaNumeros s = new B5_SumaNumeros();
+        System.out.println(s.suma(13, 28));
+    }
+
+    public int suma(int valor1, int valor2) {
+        int valorfinal = 0;
+        int i = valor1 + 1;
+        while (i < valor2) {
+            if (i % 10 == 3 || i % 10 == 5)
+                valorfinal += i;
+            i++;
+        }
+        return valorfinal;
+    }
 }
