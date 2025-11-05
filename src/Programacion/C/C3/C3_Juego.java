d45b5abf195609b8da98aaa1f073264be44275c8
Marcos Garcia Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2025-11-05 10:28:11 +0100

    Refactor C3_Juego class to improve game logic and user feedback

--- src/Programacion/C/C3/C3_Juego.java
+++ src/Programacion/C/C3/C3_Juego.java
@@ -8,13 +8,14 @@ public class C3_Juego {
         long tiempoInicio = System.currentTimeMillis();
         long tiempoFin = tiempoInicio + 120000; // 2 minuto en milisegundos
         boolean fin = false;
-        if (System.currentTimeMillis() == tiempoFin) {
-            System.out.println("Se ha acabado el tiempo. Fin del juego.");
-            fin = true;
-            return;
-        }
         int contador = 0;
         while (!fin) {
+            if (System.currentTimeMillis() >= tiempoFin) {
+                System.out.println("Se ha acabado el tiempo. Fin del juego.");
+                System.out.println("Has conseguido " + contador + " aciertos.");
+                fin = true;
+                return;
+            }
             int numero1 = juego.numeroAleatorio();
             int numero2 = juego.numeroAleatorio();
             System.out.println("Números generados: " + numero1 + " y " + numero2);
@@ -22,7 +23,7 @@ public class C3_Juego {
             operacion = (int) (Math.random() * 4 + 1);
             switch (operacion) {
                 case 1 -> { // Suma
-                    System.out.println("Has elegido suma");
+                    System.out.println("Se ha elegido suma");
                     System.out.println("Operación: " + numero1 + " + " + numero2);
                     int resultadoSuma = numero1 + numero2;
                     if (juego.pedirResultado() == resultadoSuma) {
@@ -35,11 +36,12 @@ public class C3_Juego {
                     }
                 }
                 case 2 -> { // Resta
-                    System.out.println("Has elegido resta");
+                    System.out.println("Se ha elegido resta");
                     System.out.println("Operación: " + numero1 + " - " + numero2);
                     int resultadoResta = numero1 - numero2;
                     if (juego.pedirResultado() == resultadoResta) {
                         System.out.println("Correcto");
+                        contador++;
                     } else {
                         System.out.println("Incorrecto, el resultado correcto es: " + resultadoResta);
                         System.out.println("Fin del juego. Has conseguido " + contador + " aciertos.");
@@ -47,11 +49,12 @@ public class C3_Juego {
                     }
                 }
                 case 3 -> { // Multiplicacion
-                    System.out.println("Has elegido multiplicacion");
+                    System.out.println("Se ha elegido multiplicacion");
                     System.out.println("Operación: " + numero1 + " * " + numero2);
                     int resultadoMultiplicacion = numero1 * numero2;
                     if (juego.pedirResultado() == resultadoMultiplicacion) {
                         System.out.println("Correcto");
+                        contador++;
                     } else {
                         System.out.println("Incorrecto, el resultado correcto es: " + resultadoMultiplicacion);
                         System.out.println("Fin del juego. Has conseguido " + contador + " aciertos.");
@@ -59,11 +62,12 @@ public class C3_Juego {
                     }
                 }
                 case 4 -> { // Division
-                    System.out.println("Has elegido division");
+                    System.out.println("Se ha elegido division");
                     System.out.println("Operación: " + numero1 + " / " + numero2);
-                    double resultadoDivision = numero1 / numero2;
-                    if (juego.pedirResultado() == resultadoDivision) {
+                    double resultadoDivision = (double) numero1 / numero2;
+                    if (juego.pedirResultado() == Math.round(resultadoDivision)) {
                         System.out.println("Correcto");
+                        contador++;
                     } else {
                         System.out.println("Incorrecto, el resultado correcto es: " + resultadoDivision);
                         System.out.println("Fin del juego. Has conseguido " + contador + " aciertos.");
