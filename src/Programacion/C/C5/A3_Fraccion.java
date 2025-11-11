60c85258f55f0146e50a8bbfd2d688b107aa7cff
Marcos Garcia Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2025-11-11 14:38:59 +0100

    Add user input handling for fraction operations; implement Teclado class for input management

--- src/Programacion/C/C5/A3_Fraccion.java
+++ src/Programacion/C/C5/A3_Fraccion.java
@@ -11,6 +11,22 @@ public class A3_Fraccion {
 
     A3_Fraccion() {} //constructor vacio
 
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
     void mostrarFraccion () {
         System.out.println(numerador+"/"+denominador);
     }
