9b83e7d673043e305c789f8fd25dcaa11c5774f4
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-01-12 17:04:10 +0100

    Fix typo in welcome message for seat reservation program

--- src/Programacion/Curso2526/E/E6/Main.java
+++ src/Programacion/Curso2526/E/E6/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Programa de gesti��n de reservas de asientos en un autob��s.");
+        System.out.println("Programa de gestión de reservas de asientos en un autobus.");
         E6_Autobus autobus = new E6_Autobus();
         autobus.menu();
     }
