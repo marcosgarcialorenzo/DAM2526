5b6c6e33adcec6b6c5d4fcc38ac95b952b1b2664
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-01-19 17:38:30 +0100

    Add new SQL queries for university database exercises
    Added several new SQL queries to '6.3 Universidad.sql' covering tasks such as filtering by career, student enrollment dates, student name patterns, and career enrollment ranges. Also removed an unused SQL dialect mapping from the project configuration.

--- src/BasesDeDatos/6.3 Universidad/6.3 Universidad.sql
+++ src/BasesDeDatos/6.3 Universidad/6.3 Universidad.sql
@@ -10,3 +10,49 @@ el nombre de la asignatura, el numero de alumno y el nombre de la carrera.
 SELECT CODASIG, NOMASIG, NUMALUMNOS, NOMCARRERA
 FROM ASIGNATURAS, CARRERAS
 WHERE NOMCARRERA = 'INFORMATICA' AND ASIGNATURAS.CODCARRERA = CARRERAS.CODCARRERA;
+
+3) Mostrar solo la carrera de TELEMATICA mostrando el codigo de la asignatura,
+el nombre de la asignatura, el numero de alumno y el nombre de la carrera.
+
+SELECT CODASIG, NOMASIG, NUMALUMNOS, NOMCARRERA
+FROM ASIGNATURAS, CARRERAS
+WHERE NOMCARRERA = 'TELEMATICA' AND ASIGNATURAS.CODCARRERA = CARRERAS.CODCARRERA;
+
+4) Mostrar por pantalla cuando se matriculó el alumno LUIS PEREZ, mostrándo el
+DNI, NOMBRE, APELLIDOS, EDAD y la propia fecha de matricula.
+
+SELECT A.DNI, A.NOMBRE, A.APELLIDOS, A.EDAD, M.FECHAMATRICULA
+FROM ALUMNOS A, MATRICULADOS M
+WHERE A.NOMBRE = 'LUIS' AND A.APELLIDOS = 'PEREZ' AND A.DNI = M.DNI;
+
+5) Mostrar las asignaturas de la carrera de INFORMATICA que tengan más de 50
+alumnos
+
+SELECT CODASIG, NOMASIG, NUMALUMNOS
+FROM ASIGNATURAS, CARRERAS
+WHERE NUMALUMNOS > 50 AND NOMCARRERA = 'INFORMATICA' AND ASIGNATURAS.CODCARRERA = CARRERAS.CODCARRERA;
+
+6) Mostrar los alumnos cuyos apellidos empiecen por S
+
+SELECT *
+FROM ALUMNOS
+WHERE APELLIDOS LIKE 'S%';
+
+7) Mostrar los alumnos cuyos apellidos acaben por Z
+
+SELECT *
+FROM ALUMNOS
+WHERE APELLIDOS LIKE '%Z';
+
+8) Mostrar las carreras donde el numero de matriculados entre comprendidos entre
+200 y 350.
+
+SELECT *
+FROM CARRERAS
+WHERE NUMMATRICULADOS >= 200 AND NUMMATRICULADOS <= 350;
+
+9) Mostrar todas las carreras menos INFORMATICA.
+
+SELECT *
+FROM CARRERAS
+WHERE NOMCARRERA != 'INFORMATICA';
