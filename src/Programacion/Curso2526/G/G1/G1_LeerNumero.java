cb4bc58093af16539538ad2af4a058bafe576653
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-01-22 11:31:22 +0100

    Add G1_LeerNumero and Teclado classes for number input handling

---
+++ src/Programacion/Curso2526/G/G1/G1_LeerNumero.java
@@ -0,0 +1,26 @@
+package Programacion.Curso2526.G.G1;
+
+import java.io.IOException;
+
+public class G1_LeerNumero {
+    public int leerNumero() {
+        Teclado t = new Teclado();
+        while (true) {
+            System.out.println("Introduzca un número entero:");
+            try {
+                String texto = t.leerString(); // asi para que pueda leer todo y lo convierta a entero
+                return Integer.parseInt(texto);
+            } catch (NumberFormatException numeroNoValido) {
+                System.out.println("Debe dar un número entero válido.");
+            } catch (IOException errorTeclado) {
+                System.out.println("Error de entrada/salida");
+            }
+        }
+    }
+
+    public static  void main(String[] args) {
+        G1_LeerNumero G1 = new G1_LeerNumero();
+        int numero = G1.leerNumero();
+        System.out.println("El número introducido es: " + numero);
+    }
+}
