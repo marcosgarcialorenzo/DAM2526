77f95e8cf7b46b8f2a36af31c9835d48389d0cdf
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-02-05 11:48:05 +0100

    Remove extra blank line in Main.java
    Remove an unnecessary blank line in the main method of src/Programacion/Curso2526/I/I6/Main.java to tidy up formatting and maintain consistent code style.

--- src/Programacion/Curso2526/I/I6/Main.java
+++ src/Programacion/Curso2526/I/I6/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         System.out.println("Programa de gestion de biblioteca");
         I6_Biblioteca biblioteca = new I6_Biblioteca("Mi Biblioteca");
         biblioteca.cargarLibros();
