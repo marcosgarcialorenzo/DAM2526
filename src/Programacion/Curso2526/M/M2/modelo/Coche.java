29628aa2869d32b77e98d53a905c3b5a1d6f8c03
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-04-23 10:34:34 +0200

    feat(M2): añadir constructor, atributos y @Data en Coche.java

--- src/Programacion/Curso2526/M/M2/modelo/Coche.java
+++ src/Programacion/Curso2526/M/M2/modelo/Coche.java
@@ -1,5 +1,18 @@
 package Programacion.Curso2526.M.M2.modelo;
 
-public class Coche {
+import lombok.Data;
 
+@Data
+public class Coche {
+    String matricula;
+    String modelo;
+    int antiguedad;
+    double precio;
+
+    public Coche(String matricula, String modelo, int antiguedad, double precio) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.antiguedad = antiguedad;
+        this.precio = precio;
+    }
 }
