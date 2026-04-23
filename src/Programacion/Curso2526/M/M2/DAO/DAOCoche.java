5f902d2314c80ab8b4a5368873a2911a1eb58176
Marcos García Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2026-04-23 10:41:08 +0200

    feat(M2): añadir firmas de métodos a DAOCoche.java

--- src/Programacion/Curso2526/M/M2/DAO/DAOCoche.java
+++ src/Programacion/Curso2526/M/M2/DAO/DAOCoche.java
@@ -1,4 +1,19 @@
 package Programacion.Curso2526.M.M2.DAO;
 
+import Programacion.Curso2526.M.M2.modelo.Coche;
+
+import java.util.List;
+
 public interface DAOCoche {
+    boolean insertarCoche(Coche c);
+
+    boolean eliminarCoche(int id);
+
+    boolean actualizarPreciosCoches(Coche c);
+
+    List<Coche> obtenerCoches(String modelo);
+
+    double sumarPreciosCochesPorAntiguedad(Coche c);
+
+    List<String> obtenerMatriculasPorMarca(String marca);
 }
